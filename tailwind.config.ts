2241d94c016d06e3c4dbbb059429489ba88495cb
Tiago Amado Durante <tiagodurante@icloud.com>
2023-12-14 21:00:00 -0300

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss'
+import { fontFamily } from 'tailwindcss/defaultTheme'
 
 const config: Config = {
   content: [
@@ -8,10 +9,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        'bio-green': '#016066',
+        golden: '#B9A576',
+      },
+      fontFamily: {
+        sans: ['var(--font-cairo)', ...fontFamily.sans],
       },
     },
   },
